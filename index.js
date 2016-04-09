e4a89e428d642cd3ea9c9eb6ae5a2dcf4da7b7e7
papiro <pierre.pirault@outlook.com>
2016-04-09 04:16:40 -0400

    - defect fixes - livitytest module included

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 'use strict'
 
-module.exports = () => {
-  Array.prototype.map.call(arguments, arg => {
-    switch (arg[1]) {
+const util = require('youtil')
+
+module.exports = (args) => {
+  return args.map(arg => {
+    switch (arg[0]) {
       case '[':
         return arg
               .slice(1)
-              .slice(0, arg.length-1)
+              .slice(0, arg.length-2)
               .split(',')
+              .map(val => {
+                let intVal = parseInt(val, 10)
+                return intVal == val ? intVal : val
+              })
         break;
       case '{':
-        return JSON.parse(arg)
+        return util.string2Object(arg)
         break;
       default:
         return arg
