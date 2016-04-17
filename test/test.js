ea73fc26e0772b5cadae9de6d37f42dc99777ce9
papiro <pierre.pirault@outlook.com>
2016-04-16 22:53:58 -0400

    fixed test

--- test/test.js
+++ test/test.js
@@ -1,23 +1,14 @@
 var expect = require('livitytest').expect
 ,   p = require('../index')
 
-,   args = [
-  [
-    'test'
-  , 'test'
-  , 1
+,   args = [ 
+    ['test', 'test', 1]
+  , ['[1, 2, 3]', '[one, two, three]', '{one: 1, two: 2, three: 3}']
   ]
-  ,
-  [
-    '[1, 2, 3]'
-  , '[one, two, three]'
-  , '{one: 1, two: 2, three: 3}'
-  ]
-]
 
-expect(p(args[0])).toEqual(['test', 'test', 1])
+expect(p(args[0])).equal(['test', 'test', 1])
 
-expect(p(args[1])).toEqual([
+expect(p(args[1])).equal([
   [1, 2, 3]
 , ['one', 'two', 'three']
 , {
