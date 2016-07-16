9e47c003a685c3adbe96d0b128f41d3144e6af78
papiro <pierre.pirault@outlook.com>
2016-07-15 23:57:06 -0400

    support added for "--key=value" and "-k value" type parameters.  May also do "-efG param".  Removed ability to pass stringified object in through command line.

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 'use strict'
 
-const util = require('youtil')
+const str2arr = (str) => {
+  return arg
+    .slice(1)
+    .slice(0, arg.length-2)
+    .split(/\s*,\s*/)
+    .map(val => {
+      let intVal = parseInt(val, 10)
+      return intVal == val ? intVal : val
+    })
+}
 
 module.exports = (args) => {
-  return args.map(arg => {
-    switch (arg[0]) {
-      case '[':
-        return arg
-              .slice(1)
-              .slice(0, arg.length-2)
-              .split(/\s*,\s*/)
-              .map(val => {
-                let intVal = parseInt(val, 10)
-                return intVal == val ? intVal : val
-              })
-        break;
-      case '{':
-        return util.JSONparse(arg)
-        break;
-      default:
-        return arg
-        break;
+  let obj = {}, pair = ''
+  args.forEach(arg => {
+    // if previous arg was a '-a' type
+    if (pair) {
+      obj[pair] = arg
+      pair = ''
+    }
+    // --key=value
+    else if (arg[1] === '-') {
+      const [key, val] = arg.replace(/^-{2}/, '').split('=')
+      obj[key] = val
+    } 
+    // a '-e' or '-eaG' type
+    else {
+      arg.slice(1).split('').forEach(param => {
+        obj[param] = true
+      }) 
+      pair = arg[arg.length-1]
     }
   })
+  return obj
 }
+
+module.exports(process.argv.filter(arg => arg[0] !== '/'))
